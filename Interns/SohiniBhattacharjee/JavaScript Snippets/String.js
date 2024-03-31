6f8356928fb32ced26cc8c4b00d0c63ce1bb1c10
Sohini Bhattacharjee <sohinismail@gmail.com>
2024-03-31 21:44:43 +0530

    added: JS folder

---
+++ Interns/SohiniBhattacharjee/JavaScript Snippets/String.js
@@ -0,0 +1,25 @@
+let str1 = "  Hello world";
+console.log(str1.trim());
+
+let str2 = "Sohini";
+console.log(str2.charAt(1));
+
+let str3 = "Bhattacharjee";
+
+let str4 = str2.concat(" ", str3);
+console.log(str4);
+
+console.log(str2.indexOf("i"));
+
+console.log(str2.lastIndexOf("i"));
+
+console.log(str4.slice(0,7));
+
+let a = 300;
+console.log(typeof a.toString());
+
+console.log(str4.substring(7, 20));
+
+console.log(str4.toUpperCase());
+
+console.log(str4.toLowerCase());
