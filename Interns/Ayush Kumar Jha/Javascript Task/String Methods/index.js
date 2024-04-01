0303c819e33cb96b82cd903a3b39440c99fdfc53
Ayush kumar Jha <ayushjha298@gmail.com>
2024-04-01 07:53:12 +0530

    new commit

---
+++ Interns/Ayush Kumar Jha/Javascript Task/String Methods/index.js
@@ -0,0 +1,39 @@
+// Example string
+let str = "Ayush, Jha";
+
+// Method: toUpperCase()
+console.log("toUpperCase():", str.toUpperCase());
+
+// Method: toLowerCase()
+console.log("toLowerCase():", str.toLowerCase());
+
+// Method: concat()
+console.log("concat():", str.concat(" I'm here!"));
+
+// Method: indexOf()
+console.log("indexOf('Jha'):", str.indexOf("Jha"));
+
+// Method: slice()
+console.log("slice(1, 4):", str.slice(1, 4));
+
+// Method: substring()
+console.log("substring(1, 3):", str.substring(1, 3));
+
+// Method: replace()
+console.log("replace('Jha', 'Kumar'):", str.replace("Jha", "Kumar"));
+
+// Method: split()
+console.log("split(','): ", str.split(","));
+
+// Method: charAt()
+console.log("charAt(7):", str.charAt(7));
+
+// Method: trim()
+let whitespaceStr = "   Trim me!   ";
+console.log("trim():", whitespaceStr.trim());
+
+let st = "a";
+console.log(typeof(+st));
+
+let num = 10;
+console.log(typeof(num+" "));
